719a0adeef28626acaeaf1ba64dbaf56a78af438
Heiko Damaske <heiko.damaske@gmail.com>
2024-02-11 21:12:39 +0100

    added user agent

--- core/scrapy_project/settings.py
+++ core/scrapy_project/settings.py
@@ -104,3 +104,5 @@ ROBOTSTXT_OBEY = False
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36'
+

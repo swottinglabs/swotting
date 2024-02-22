cc61a05df21f23abdaab8835adffb8b1c67a8f21
root <root@DESKTOP-041KPBA>
2024-02-22 23:31:01 +0100

    save course to csv file and loop through through all pages

--- core/scrapy_project/settings.py
+++ core/scrapy_project/settings.py
@@ -104,4 +104,3 @@ ROBOTSTXT_OBEY = False
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
-
--- core/scrapy_project/spiders/classCentralProviders.py
+++ core/scrapy_project/spiders/classCentralProviders.py
@@ -2,14 +2,16 @@ from pathlib import Path
 import scrapy
 import json
 import re
+import csv
 from bs4 import BeautifulSoup
 
 # Todo to run: active the virtual environment and run the following command python3 manage.py scrape classCentralProviders in the ubuntu
 # TODO
 # provider_name  is not working - DONE
+# Save courses to a csv file. - DONE
 # Write a function to get the link that is being requested. 
-# make 1-5 sec delay between requests
-# make that it loops through all the pages. 
+# make 1-5 sec delay between requests - Done
+# make that it loops through all the pages. - Done
 
 
 class ProvidersSpider(scrapy.Spider):
@@ -20,6 +22,8 @@ class ProvidersSpider(scrapy.Spider):
     custom_settings = {
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
     }
+
+    download_delay = 2.0
 
     def parse(self, response):
         # Delegate the processing of providers to the get_providers function
@@ -47,7 +51,6 @@ class ProvidersSpider(scrapy.Spider):
         for request in self.getAllLinksFromProviders(providers):
             yield request
             
-
     def parse_course_data(self, response):
         # Extract the text content of the response object
         input_json_string = response.text  # or response.body.decode('utf-8') if you encounter encoding issues
@@ -57,21 +60,24 @@ class ProvidersSpider(scrapy.Spider):
 
         # Extract the provider slug from the meta data
         provider_slug = response.meta['provider_slug']
-        
+
         # Save the loaded JSON data to a file for testing
         with open('input_data.json', 'w') as file:
             json.dump(data, file, indent=4)
-        
+
         # Proceed with parsing the HTML content
         soup = BeautifulSoup(data['table'], 'html.parser')
-        
+
         # Initialize an empty list to store course information
         courses_info = []
-        
-        # Find all course elements in the parsed HTML using the updated class
-        courses = soup.find_all('li', class_='course-list-course')
 
+        # Find all course elements in the parsed HTML
+        courses = soup.find_all('li', class_='course-list-course')
         print("Found this many courses: ", len(courses))
+
+        if not courses:
+            print("No courses found")
+            return
 
         for course in courses:
             # Extract course information
@@ -80,27 +86,56 @@ class ProvidersSpider(scrapy.Spider):
             provider_name = course.find('span', text='Provider').find_next_sibling('a').text if course.find('span', text='Provider') else None
             summary = course.find('p', class_='text-2 margin-bottom-xsmall').text.strip() if course.find('p', class_='text-2 margin-bottom-xsmall') else None
             duration = None
-            # Search directly for the span element with the specific aria-label.
             duration_element = course.find('span', {'aria-label': 'Workload and duration'})
             if duration_element:
-                duration = duration_element.text.strip()  # Use strip() to clean any extra whitespace
-            # Append the extracted information as a dictionary to the courses_info list
+                duration = duration_element.text.strip()
+
+            # Rename 'course_url' to 'url_classCentral'
             courses_info.append({
                 'course_name': course_name,
-                'course_url': course_url,
+                'url_classCentral': course_url,
                 'provider_slug': provider_slug,
                 'summary': summary,
                 'duration': duration
             })
 
         print("Finished parsing course data and found this many courses: ", len(courses_info))
-        
-        # Save the courses information to another JSON file
-        with open('courses_info.json', 'w') as file:
-            json.dump(courses_info, file, indent=4)
-        
-        return courses_info
 
+        # Read the existing CSV to check for duplicates
+        existing_urls = set()
+        try:
+            with open('courses_info.csv', mode='r', newline='', encoding='utf-8') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    existing_urls.add(row['url_classCentral'])
+        except FileNotFoundError:
+            # If the file does not exist, proceed to create it
+            pass
+
+        # Save the courses information to a CSV file, excluding duplicates
+        with open('courses_info.csv', mode='a', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['course_name', 'url_classCentral', 'provider_slug', 'summary', 'duration']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            
+            # Write the header only if the file is newly created
+            if not existing_urls:
+                writer.writeheader()
+
+            for course in courses_info:
+                if course['url_classCentral'] not in existing_urls:
+                    writer.writerow(course)
+
+        current_page = response.meta['current_page']
+        print(f"Current page complete: {current_page}")
+        # For testing if current page over 4 then stop
+        if current_page > 4:
+            return courses_info
+
+        modified_url = f"https://www.classcentral.com/maestro/provider/{provider_slug}?page={current_page+1}&free=true"
+        print(f"Modified Url: {modified_url}")
+        yield scrapy.Request(url=modified_url, callback=self.parse_course_data, meta={'provider_slug': provider_slug, 'current_page': current_page + 1})
+
+    
     def extract_course_details(self, course_html):
         # This is a simplified extraction method
         # You'll need to parse HTML or use regex to extract the actual details
@@ -130,14 +165,14 @@ class ProvidersSpider(scrapy.Spider):
 
             # Split the URL to get the provider name slug
             provider_slug = provider['url'].split('/')[-1]
-            # Create the new URL format
+            # Create the new URL format start at page 1 and only free courses
             modified_url = f"https://www.classcentral.com/maestro/provider/{provider_slug}?page=1&free=true"
 
             print(f"Modified Url: {modified_url}")
 
             # Schedule a request to the modified URL
             # Use the callback parameter to specify the method that will handle the response
-            yield scrapy.Request(url=modified_url, callback=self.parse_course_data, meta={'provider_slug': provider_slug})
+            yield scrapy.Request(url=modified_url, callback=self.parse_course_data, meta={'provider_slug': provider_slug, 'current_page': 1})
 
 
 

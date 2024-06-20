a697f6da531d43946d5592f97b15c61e81bc24a1
Peter Turner <software@peterturner.co.za>
2024-06-20 20:50:38 +0200

    Add fix to include commands, add index.html

--- swotting/settings.py
+++ swotting/settings.py
@@ -86,15 +86,15 @@ WSGI_APPLICATION = 'swotting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
-#     'default': dj_database_url.config(default='postgres://USER:PASSWORD@HOST:PORT/DB_NAME', conn_max_age=600)
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://USER:PASSWORD@HOST:PORT/DB_NAME', conn_max_age=600)
+}
 
 
 

126435494cf637560484ce70c1cb43e48b035a9e
Peter Turner <software@peterturner.co.za>
2024-06-20 19:17:48 +0200

    Add fix to include commands, add index.html

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns = [
@@ -6,4 +8,8 @@ urlpatterns = [
     path('categories', views.DigitalLearningResourceCategoryListCreateView.as_view(), name='categories'),
     path('courses', views.DigitalLearningResourceListCreateView.as_view(), name='courses'),
     path('courses/<int:id>', views.DigitalLearningResourceDetailView.as_view(), name='course'),
+
+    # Landing page
+    path('', TemplateView.as_view(template_name="core/index.html"), name='index'),
+
 ]

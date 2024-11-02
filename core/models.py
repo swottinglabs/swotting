9144a98ee4458e636f2f419ee193b7aa24c2b7a8
heikdam <heik.damaske@gmail.com>
2024-11-02 22:42:33 +0100

    update

--- core/models.py
+++ core/models.py
@@ -23,11 +23,10 @@ class Creator(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     url = models.URLField(max_length=255, blank=True, null=True)
-    platformCreatorId = models.CharField(max_length=255, blank=True, null=True)
+    platform_creator_id = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     type = models.ForeignKey(CreatorType, on_delete=models.SET_NULL, null=True)
-    platformThumbnailUrl = models.URLField(max_length=255, blank=True, null=True)
-    thumbnailUrl = models.URLField(max_length=255, blank=True, null=True)
+    platform_thumbnail_url = models.URLField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -55,7 +54,7 @@ class Format(models.Model):
 
 
 class Language(models.Model):
-    isoCode = models.CharField(max_length=10, primary_key=True)
+    iso_code = models.CharField(max_length=10, primary_key=True)
     name = models.CharField(max_length=255)
 
     def __str__(self):
@@ -65,8 +64,6 @@ class Language(models.Model):
 class Tag(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    level = models.IntegerField(help_text="1 is the highest level, 2 is sub-category")
-    parentTag = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -74,29 +71,31 @@ class Tag(models.Model):
 
 class LearningResource(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    scraped_timestamp = models.DateTimeField(auto_now_add=True)
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
-    platformId = models.ForeignKey(Platform, on_delete=models.CASCADE)
-    platformCourseId = models.CharField(max_length=255)
+    platform_id = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    platform_course_id = models.CharField(max_length=255)
     url = models.URLField(max_length=255)
     name = models.CharField(max_length=255)
+    short_description = models.TextField()
     description = models.TextField()
+    html_description = models.TextField(blank=True, null=True)
     languages = models.ManyToManyField(Language)
-    isFree = models.BooleanField(default=True)
-    isLimitedFree = models.BooleanField(default=False)
-    dollarPrice = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    hasCertificate = models.BooleanField(default=False)
-    certificateDollarPrice = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    is_free = models.BooleanField(default=True)
+    is_limited_free = models.BooleanField(default=False)
+    dollar_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    has_certificate = models.BooleanField(default=False)
     creators = models.ManyToManyField(Creator)
     formats = models.ManyToManyField(Format)
     tags = models.ManyToManyField(Tag)
     level = models.ForeignKey(Level, on_delete=models.SET_NULL, null=True)
-    platformLastMod = models.DateTimeField(null=True, blank=True)
-    platformThumbnailUrl = models.URLField(max_length=255, blank=True, null=True)
-    thumbnailUrl = models.URLField(max_length=255, blank=True, null=True)
+    platform_last_update = models.DateTimeField(null=True, blank=True)
+    platform_thumbnail_url = models.URLField(max_length=255, blank=True, null=True)
     duration = models.DurationField(null=True, blank=True)
-    reviewsCount = models.PositiveIntegerField(default=0)
-    reviewsRating = models.DecimalField(
+    platform_reviews_count = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+    enrollment_count = models.PositiveIntegerField(default=0)
+    platform_reviews_rating = models.DecimalField(
         max_digits=3, 
         decimal_places=2, 
         validators=[MinValueValidator(1), MaxValueValidator(5)],

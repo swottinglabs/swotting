e1034274385234eead8785c9fb31a21c5264c2e5
heikdam <heik.damaske@gmail.com>
2025-03-02 22:54:18 +0100

    added new digitial ocean ip

--- swotting/settings.py
+++ swotting/settings.py
@@ -33,9 +33,10 @@ SECRET_KEY = 'django-insecure-c2^2=c-4f607+i#(ubo4$@t_-clm_1_)spak6pkkn^u3t5x@q5
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '165.22.29.100',  # Add your server's IP address
-    'localhost',  # Useful for local testing
-    '127.0.0.1',  # Useful for local testing
+    '165.22.29.100',  # Peter's server
+    '143.110.209.181', # Heiko's server
+    'localhost', 
+    '127.0.0.1',  
     'api.swotting.org',
     'swotting.org',
     '*'
